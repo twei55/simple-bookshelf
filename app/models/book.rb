607490ae3c4e6b14f3fe3d5a1b3b41380c3c07b0
Tobias Weiß <tobias.weiss@gmail.com>
2012-06-04 21:40:53 +0200

    Updated paperclip url and path options

--- app/models/book.rb
+++ app/models/book.rb
@@ -46,8 +46,8 @@ class Book < ActiveRecord::Base
   ##############
 
   has_attached_file :document, 
-    :url => "/system/books/documents/:id/original/:basename_:style.:extension",
-    :path => ":rails_root/public/system/books/documents/:id/original/:basename_:style.:extension"
+    :url => "/system/books/documents/:id/original/:basename.:extension",
+    :path => ":rails_root/public/system/books/documents/:id/original/:basename.:extension"
 
   ##############
   # Thinking Sphinx

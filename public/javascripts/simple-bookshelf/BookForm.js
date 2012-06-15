b033f7efde5facee10571589d94ed2f29cab6b31
Tobias Weiß <tobias.weiss@gmail.com>
2012-06-15 15:01:41 +0200

    Refactored JavaScript code to work with jquery

--- public/javascripts/simple-bookshelf/BookForm.js
+++ public/javascripts/simple-bookshelf/BookForm.js
@@ -1,57 +1,40 @@
-function removeAuthor (link) {
-	console.log(link);
-	$(link).previous("input[type=hidden]").value = "1";
-	$(link).up(".fields").hide();
+function removeAuthor(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
 }
 
-function addNewAuthor (link) {
-	var clone = $('new_author_fields').innerHTML;
-	var new_id = new Date().getTime();	// generate unique index
-	var regexp = new RegExp("new_authors", "g");
-	clone = clone.replace(regexp, new_id);
-	$(link).up().insert({
-		before: clone
-	});
-  }
+function addNewAuthor(link) {
+	var clone = $('div#new_author_fields').html();
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_author_fields", "g");
+  $(link).parent().before(clone.replace(regexp, new_id));
+}
 
-//
-var BookForm = Class.create ({
+var BookForm = {
 	
 	observeTitleField : function () {
-		$('book_title').observe('keyup',function(evt) {
-			if ($F('book_title').length > 2 && $F('book_title').endsWith(" ")) {
-				this.getBooksWithSimilarTitles();
+		$('#book_title').keyup(function(evt) {
+			str = $('#book_title').val();
+			if (str.length > 2 && str[str.length-1] == " ") {
+				this.getBooksWithSimilarTitles(str.trim());
 			}
 		}.bind(this));
 	},
 	
-	getBooksWithSimilarTitles : function () {
-		var url = "/books/similar";
-		new Ajax.Request(url, {
-			method: 'get',
-			parameters: "title=" + $F('book_title'),
-			onSuccess: this.onListSuccess.bind(this),
-			onFailed: this.onListFailed.bind(this),
-			onLoading: this.onListLoading.bind(this)
-		});
+	getBooksWithSimilarTitles : function (str) {
+		var jqxhr = $.ajax("/books/similar?title=" + str);
+		jqxhr.success(this.showSimilarBooks);
 	},
-	
-	onListSuccess : function (res) {
-		if (res && !res.responseText.empty()) {
-			$('similar_books').update(res.responseText);
-			$('similar_books').show();
+
+	showSimilarBooks : function (data, textStatus, jqXHR) {
+		console.log(data);
+		if (data) {
+			$('div#similar_books').html(data);
+			$('div#similar_books').show();
 		}
 		else {
-			$('similar_books').update("");
-			$('similar_books').hide();	
+			$('div#similar_books').html("");
+			$('div#similar_books').hide();
 		}
-	},
-	
-	onListFailed : function (res) {
-		
-	},
-	
-	onListLoading : function (res) {
-		
 	}
-});
+};

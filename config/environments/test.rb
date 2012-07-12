2f407ddea5c1f0a77e8a896197683dc73f7479a1
Tobias Weiß <tobias.weiss@gmail.com>
2012-07-12 23:01:18 +0200

    Added default_url_options to test config

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,6 +33,8 @@ SimpleBookshelf::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
 
+  config.action_mailer.default_url_options = { :host => "localhost", }
+
   # ruby-debug settings
   Debugger.settings[:autoeval] = true
 end
